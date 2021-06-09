7567c1ad2864dbd11f0812ea7f70c159a243861f
MouawadK <59969797+MouawadK@users.noreply.github.com>
2021-06-08 22:31:23 -0400

    Updated CardTrick.Java
    Was having a hard time trying to figure out why I'm getting so many different errors trying to push into this branch. So I just created my personal repository in my Git and then edited & pasted the code here....

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
@@ -9,24 +9,87 @@ package ca.sheridancollege.week3.softwarefundamentals.exercise1;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author dancye
+ * @author Khalil Mouawad
+ * 2021-06-08
+ */
+package Khalil_Mouawad_Exercise1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Khalil Mouawad
+ * 2021-06-08
  */
 public class CardTrick {
-    
     public static void main(String[] args)
     {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
+      
+      int value;
+      String suit;
+      
+     
+          
+      Card[] magicHand = new Card[7];
     
-}
+     
+      for (int i=0; i<magicHand.length; i++){
+          Card card = new Card();
+          Random random = new Random();
+          
+          card.setValue((int)(Math.random()*13)+1);
+          
+          magicHand[i] = card;
+      }
+      
+      Scanner sc = new Scanner(System.in);
+      Card user = new Card();
+
+      
+      System.out.println("Please pick any card value: ");
+      value = sc.nextInt();
+      System.out.println("Please pick a card suit: ");
+      suit = sc.next();
+      
+      user.setValue(value);
+      user.setSuit(suit);
+      
+            boolean match = false;
+            for (int i=0; i<magicHand.length; i++) {
+                if (user.getValue() == magicHand[i].getValue() &&
+                        user.getSuit().equalsIgnoreCase(magicHand[i].getSuit()))
+                {
+                    match = true;
+                    break;
+                }
+            }
+                    if (match) {
+                        System.out.println("Your card choice already exists! ");
+                    }
+            
+                else
+                        
+          System.out.println("Your choice: "+value+" of "+suit);
+      Card favoredCard = new Card();
+      favoredCard.setValue(10);
+      favoredCard.setSuit(Card.SUIT[0]);
+      
+      for (int i = 0; i<magicHand.length; i++){
+          if (favoredCard.getValue() == magicHand[i].getValue() && 
+                favoredCard.getSuit().equalsIgnoreCase(magicHand[i].getSuit()))
+                                                 {
+                                                       match = true;
+                                                       break;
+                                                }
+                                          }
+     
+      if (match) {
+          System.out.println("You found the card! ");
+          
+          
+      } else
+          System.out.println("The card was not found, please try again.");
+      } 
+     }
