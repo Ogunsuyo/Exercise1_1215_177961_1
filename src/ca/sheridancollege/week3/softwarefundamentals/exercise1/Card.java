671ac4ea1a46fd68f11d3c388a8af8d86fbc455c
MouawadK <59969797+MouawadK@users.noreply.github.com>
2021-06-08 22:32:26 -0400

    Update Card.java

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/Card.java
@@ -14,40 +14,44 @@ package ca.sheridancollege.week3.softwarefundamentals.exercise1;
  * add your name as a modifier.
  * @author dancye
  */
+/**
+ *
+ * @author Khalil Mouawad
+ * 2021-06-08
+ */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+    private String suit; //Hearts,Spades,Diamonds,Clubs
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+        private int value; //1-13
+        
+        public static final String[]SUIT= {"Hearts", "Diamonds", "Spades", "Clubs"};
     /**
-     * @return the suit
+     *
+     * @return
      */
-    public String getSuit() {
-        return suit;
-    }
-
-    /**
+        public String getSuit() {
+            return suit;
+        }
+     /**
+     *
      * @param suit the suit to set
      */
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
-
+        public void setSuit(String suit) {
+            this.suit = suit;
+     } 
     /**
+     *
      * @return the value
      */
-    public int getValue() {
+        public int getValue() {
         return value;
-    }
-
-    /**
+       }
+     /**
+     *
      * @param value the value to set
-     */
-    public void setValue(int value) {
-        this.value = value;
-    }
-   
-   
-    
-}
+     */ 
+        public void setValue(int value) {
+            this.value = value;
+        }
+   }
