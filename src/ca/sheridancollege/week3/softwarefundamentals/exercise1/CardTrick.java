0ec8b1f6197541365e14264505fd924a1ff220b9
Ogunsuyo <78033763+Ogunsuyo@users.noreply.github.com>
2021-06-10 19:24:40 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/exercise1/CardTrick.java
@@ -11,6 +11,11 @@ package ca.sheridancollege.week3.softwarefundamentals.exercise1;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
  */
+// Oluwatoyin Ogunsuyi
+// Prof asidhu
+// June 10 2021
+// Software Fundamentals
+
 public class CardTrick {
     
     public static void main(String[] args)
@@ -19,14 +24,43 @@ public class CardTrick {
         
         for (int i=0; i<magicHand.length; i++)
         {
+            Random rnd = new Random();
             Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue(rnd.nextInt(13)+1);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[rnd.nextInt(3)]);
+            magicHand[i]= c;
+            System.out.println(c.getValue());
+            System.out.println(c.getSuit());
         }
         
         //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
+        System.out.println("Enter Your Card Value and Card Suit");
+       System.out.println("Card Value:");
+       int cardNumber = sc.nextInt();
+       System.out.println("Suit Value:");
+        String suit = sc.nextLine();
+        boolean doesExist = false;
+        
+        
+        // and search magicHand here  
+        for (int i= 0; i < magicHand.length; i++) {
+          if(magicHand[i].getValue()== cardNumber && magicHand[i].getSuit()== suit){
+                 doesExist = true;
+             }
+        
+        
+        
         //Then report the result here
+ if(doesExist) 
+         {
+            System.out.println("The Card exist in your hand");
+        }
+       else
+       {
+           System.out.println("The Card does not exist in your hand");  
+       }
+     
+        } 
     }
-    
-}
